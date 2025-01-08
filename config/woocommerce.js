c62bbfb372530cd3ddb658ff890d1272c6617c43
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2025-01-08 12:18:03 +0530

    Env Update

--- config/woocommerce.js
+++ config/woocommerce.js
@@ -1,10 +1,13 @@
 import pkg from '@woocommerce/woocommerce-rest-api';
 const WooCommerceRestApi = pkg.default;
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const WooCommerce = new WooCommerceRestApi({
-  url: "https://lelechicks.com",
-  consumerKey: "ck_50836d6e06adbfa3f5be997b2d5cac199a0d9b85",
-  consumerSecret: "cs_95b5eb7c1a6af2571e59ce822452d0894b5bd21d",
+  url: process.env.WOOCOMMERCE_URL,
+  consumerKey: process.env.WOOCOMMERCE_CONSUMER_KEY,
+  consumerSecret: process.env.WOOCOMMERCE_CONSUMER_SECRET,
   version: 'wc/v3'
 });
 
