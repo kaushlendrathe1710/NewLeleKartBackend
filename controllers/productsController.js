e20dc5d4de2047548fdabdef85dcd12b2995a60f
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2025-01-09 13:20:56 +0530

    Refactor getProductById to use async/await and include product variations in response

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -12,13 +12,28 @@ export const getProducts = (req, res) => {
     });
 };
 
-export const getProductById = (req, res) => {
+export const getProductById = async (req, res) => {
   const { id } = req.params;
-  WooCommerce.get(`products/${id}`)
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      res.status(error.response.status).send(error.response.data);
-    });
+
+  try {
+    // Fetch product details
+    const productResponse = await WooCommerce.get(`products/${id}`);
+    const product = productResponse.data;
+
+    // Fetch product variations (if any)
+    let variations = [];
+    if (product.variations.length > 0) {
+      const variationsResponse = await WooCommerce.get(
+        `products/${id}/variations`
+      );
+      variations = variationsResponse.data;
+    }
+
+    // Send combined response
+    res.send({ product, variations });
+  } catch (error) {
+    res
+      .status(error.response?.status || 500)
+      .send(error.response?.data || error.message);
+  }
 };
