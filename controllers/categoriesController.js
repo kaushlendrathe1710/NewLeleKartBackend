9015bf0e8d3a53dcee5a98665ea6b4789d0631e9
Ayush Chauhan <ayush.chuahan.22cse@bmu.edu.in>
2025-01-10 12:58:36 +0530

    Add forgot password functionality and subcategory retrieval endpoint

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -1,7 +1,7 @@
 import WooCommerce from '../config/woocommerce.js';
 
 export const getCategories = (req, res) => {
-  const queryParams = req.query;
+  const queryParams = { ...req.query, parent: 0 };
   const pageNumber = parseInt(queryParams.page || 1);
   WooCommerce.get('products/categories', queryParams)
     .then((response) => {
@@ -18,6 +18,17 @@ export const getCategories = (req, res) => {
 export const getCategoryById = (req, res) => {
   const { id } = req.params;
   WooCommerce.get(`products/categories/${id}`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.status(error.response.status).send(error.response.data);
+    });
+};
+
+export const getSubcategories = (req, res) => {
+  const { parentId } = req.params;
+  WooCommerce.get('products/categories', { parent: parentId })
     .then((response) => {
       res.send(response.data);
     })
--- routes/categoriesRoutes.js
+++ routes/categoriesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCategories, getCategoryById } from '../controllers/categoriesController.js';
+import { getCategories, getCategoryById, getSubcategories } from '../controllers/categoriesController.js';
 
 const router = express.Router();
 
 router.get('/', getCategories);
 router.get('/:id', getCategoryById);
+router.get('/:parentId/subcategories', getSubcategories);
 
 export default router;
