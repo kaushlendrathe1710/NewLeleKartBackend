a30b9d155303db5796d79a60e252875f3a72e180
manojmksmanu <manoj2022019@gmail.com>
2025-01-13 21:31:14 +0530

    port changed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors"; // Import the cors package
-import productRoutes from "./routes/productsRoutes.js";
-import productAttributesRoutes from "./routes/productAttributesRoutes.js";
-import categoriesRoutes from "./routes/categoriesRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-import WooCommerce from "./config/woocommerce.js";
-import customerRoutes from "./routes/customerRoutes.js";
+import express from 'express';
+import cors from 'cors'; // Import the cors package
+import productRoutes from './routes/productsRoutes.js';
+import productAttributesRoutes from './routes/productAttributesRoutes.js';
+import categoriesRoutes from './routes/categoriesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import WooCommerce from './config/woocommerce.js';
+import customerRoutes from './routes/customerRoutes.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 // Enable CORS for all routes
 app.use(cors());
@@ -17,11 +17,11 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/products", productRoutes);
-app.use("/attributes", productAttributesRoutes);
-app.use("/categories", categoriesRoutes);
-app.use("/auth", authRoutes);
-app.use("/customers", customerRoutes);
+app.use('/products', productRoutes);
+app.use('/attributes', productAttributesRoutes);
+app.use('/categories', categoriesRoutes);
+app.use('/auth', authRoutes);
+app.use('/customers', customerRoutes);
 
 // Start the server
 app.listen(PORT, () => {
